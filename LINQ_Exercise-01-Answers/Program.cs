6ea08390ea22f3592d6644f3db5ba99c0b932d5f
johndward01 <johndward01@gmail.com>
2022-05-17 16:52:40 -0500

    added console app

---
+++ LINQ_Exercise-01-Answers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_Exercise_01_Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
