5e887d15ad0916975bc76d67e20831937c631008
johndward01 <johndward01@gmail.com>
2022-05-17 17:25:28 -0500

    added answer and TODO question

--- LINQ_Exercise-01-Answers/Program.cs
+++ LINQ_Exercise-01-Answers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQ_Exercise_01_Answers
 {
@@ -6,7 +8,60 @@ namespace LINQ_Exercise_01_Answers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //****Collab with classmates***** 
+
+            //TODO: Look up new data structure  (each person in your group should try to use a different type) 
+            //TODO: Instantiate an object of that type
+            //TODO: Insert at least 10 entries into data structure (object)          
+            //TODO: Put yourselves in the shoes of a teacher, create a LINQ situation/question and use one of the LINQ methods below to solve your own question
+            //Example: Using the dictionary below, filter out all of the entries with vowels
+            //Hint: Try creating queries (questions) that pertain to the data structure of your choosing
+            //BONUS: Try using a LINQ method not in the list below for extra practice! 
+
+
+            //Example data structure: 
+            var dictionary = new Dictionary<int, string>
+            {
+                { 1, "abc" },
+                { 2, "def" },
+                { 3, "ghi" },
+                { 4, "jkl" },
+                { 5, "mno" },
+                { 6, "pqr" },
+                { 7, "stu" },
+                { 8, "vwx" },
+                { 9, "yz_" },
+                { 10, "111" }
+            };
+            // TODO: Using the dictionary above remove the entry with all number 1's
+            dictionary.Where(kvp => kvp.Value.Contains("1"))
+                .ToList()
+                .ForEach(x => Console.WriteLine($"Key: {x.Key}\tValue: {x.Value}"));
+            
+
+            
+
+
+
+
+            #region Linq Methods to choose from           
+
+            //*BONUS*: Use Linq method not in list for an extra challenge. Do your own research!          
+
+            //Where() - Filters a sequence of values based on a predicate.
+            //Select() - Projects each element of a sequence into a new form.
+            //OrderBy() - Sorts the elements of a sequence in ascending order.
+            //OrderByDescending() - Sorts the elements of a sequence in descending order.
+            //Sum() - Sums all elements
+            //Average() - Calculates average of all elements
+            //Count() - Returns the number of elements in a sequence.
+            //Min() - Finds min value in list / sequence
+            //Max() - Finds max value in list / sequence
+            //Take() - Returns a specified number of contiguous elements from the start of a sequence.
+            //Append() - Appends a value to the end of the sequence.
+            //ThenBy() - Performs a subsequent ordering of the elements in a sequence in ascending order.
+
+            #endregion
         }
     }
 }
